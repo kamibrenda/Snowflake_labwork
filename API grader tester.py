3a101329aeb130f54e6a60e14b116642801ba7e1
kamibrenda <mukamibrenda254@gmail.com>
2023-12-21 16:49:35 +0300

    Grader update code

--- API grader tester.py
+++ API grader tester.py
@@ -1,10 +1,117 @@
-select grader(step, (actual = expected), actual, expected, description) as graded_results from
-(SELECT 
- 'DORA_IS_WORKING' as step
- ,(select 123) as actual
- ,123 as expected
- ,'Dora is working!' as description
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from (
+ SELECT
+ 'DWW01' as step
+ ,( select count(*)  
+   from GARDEN_PLANTS.INFORMATION_SCHEMA.SCHEMATA 
+   where schema_name in ('FLOWERS','VEGGIES','FRUITS')) as actual
+  ,3 as expected
+  ,'Created 3 Garden Plant schemas' as description
 ); 
+
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from (
+ SELECT 'DWW02' as step 
+ ,( select count(*) 
+   from GARDEN_PLANTS.INFORMATION_SCHEMA.SCHEMATA 
+   where schema_name = 'PUBLIC') as actual 
+ , 0 as expected 
+ ,'Deleted PUBLIC schema.' as description
+); 
+
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from (
+ SELECT 'DWW03' as step 
+ ,( select count(*) 
+   from GARDEN_PLANTS.INFORMATION_SCHEMA.TABLES 
+   where table_name = 'ROOT_DEPTH') as actual 
+ , 1 as expected 
+ ,'ROOT_DEPTH Table Exists' as description
+); 
+
+
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from (
+ SELECT 'DWW04' as step
+ ,( select count(*) as SCHEMAS_FOUND 
+   from UTIL_DB.INFORMATION_SCHEMA.SCHEMATA) as actual
+ , 2 as expected
+ , 'UTIL_DB Schemas' as description
+); 
+
+
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from (
+ SELECT 'DWW05' as step
+ ,( select count(*) 
+   from GARDEN_PLANTS.INFORMATION_SCHEMA.TABLES 
+   where table_name = 'VEGETABLE_DETAILS') as actual
+ , 1 as expected
+ ,'VEGETABLE_DETAILS Table' as description
+); 
+
+
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from ( 
+ SELECT 'DWW06' as step 
+,( select row_count 
+  from GARDEN_PLANTS.INFORMATION_SCHEMA.TABLES 
+  where table_name = 'ROOT_DEPTH') as actual 
+, 3 as expected 
+,'ROOT_DEPTH row count' as description
+);  
+
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from (
+ SELECT 'DWW07' as step
+ ,( select row_count 
+   from GARDEN_PLANTS.INFORMATION_SCHEMA.TABLES 
+   where table_name = 'VEGETABLE_DETAILS') as actual
+ , 41 as expected
+ , 'VEG_DETAILS row count' as description
+); 
+
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from ( 
+   SELECT 'DWW08' as step 
+   ,( select count(*) 
+     from GARDEN_PLANTS.INFORMATION_SCHEMA.FILE_FORMATS 
+     where FIELD_DELIMITER =',' 
+     and FIELD_OPTIONALLY_ENCLOSED_BY ='"') as actual 
+   , 1 as expected 
+   , 'File Format 1 Exists' as description 
+); 
+
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from (
+ SELECT 'DWW09' as step
+ ,( select count(*) 
+   from GARDEN_PLANTS.INFORMATION_SCHEMA.FILE_FORMATS 
+   where FIELD_DELIMITER ='|' 
+   ) as actual
+ , 1 as expected
+ ,'File Format 2 Exists' as description
+); 
+
+
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from (
+ SELECT 'DWW10' as step
+  ,( select count(*) 
+    from UTIL_DB.INFORMATION_SCHEMA.stages
+    where stage_url='s3://uni-lab-files' 
+    and stage_type='External Named') as actual
+  , 1 as expected
+  , 'External stage created' as description
+ ); 
+
+ select GRADER(step, (actual = expected), actual, expected, description) as graded_results from (
+  SELECT 'DWW11' as step
+  ,( select row_count 
+    from GARDEN_PLANTS.INFORMATION_SCHEMA.TABLES 
+    where table_name = 'VEGETABLE_DETAILS_SOIL_TYPE') as actual
+  , 42 as expected
+  , 'Veg Det Soil Type Count' as description
+ ); 
+ 
+
+
+
+
+
+
+
+
 
 
 #To show all the functions associated in an account i.e ACCOUNTADMIN or SYSADMIN
