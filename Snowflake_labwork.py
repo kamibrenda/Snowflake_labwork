b9eb61ae8c55cb73985e1864405472b6a05a2429
kamibrenda <mukamibrenda254@gmail.com>
2023-12-21 10:28:22 +0300

    API intergration grader

---
+++ Snowflake_labwork.py
@@ -0,0 +1,29 @@
+Create an API Integration
+You don't have to understand what it means to "create an API Integration," you just need to run the code below (using the ACCOUNTADMIN ROLE!). 
+
+
+use role accountadmin;
+
+create or replace api integration dora_api_integration
+api_provider = aws_api_gateway
+api_aws_role_arn = 'arn:aws:iam::321463406630:role/snowflakeLearnerAssumedRole'
+enabled = true
+api_allowed_prefixes = ('https://awy6hshxy4.execute-api.us-west-2.amazonaws.com/dev/edu_dora');
+
+🧰 Create the GRADER Function
+You don't have to understand what it means to "create the GRADER function," you just need to run the code below (using the ACCOUNTADMIN ROLE!). 
+
+
+use role accountadmin;  
+
+create or replace external function util_db.public.grader(
+      step varchar
+    , passed boolean
+    , actual integer
+    , expected integer
+    , description varchar)
+returns variant
+api_integration = dora_api_integration 
+context_headers = (current_timestamp,current_account, current_statement) 
+as 'https://awy6hshxy4.execute-api.us-west-2.amazonaws.com/dev/edu_dora/grader'
+; 
