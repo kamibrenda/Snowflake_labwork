1e1d6475e9696b112be31ed562a140b9c5370cd9
kamibrenda <mukamibrenda254@gmail.com>
2024-01-02 15:43:10 +0300

    Ext to Int Loading

--- Loading data.py
+++ Loading data.py
@@ -1,12 +1,24 @@
---creating a table
-create or replace table vegetable_details_soil_type(
-plant_name varchar (25),
-soil_type number(1,0) 
+  
+--creating Soil type data table 
+CREATE OR REPLACE table LU_SOIL_TYPE(
+SOIL_TYPE_ID number (2),
+SOIL_TYPE varchar (15),
+SOIL_DESCRIPTION varchar (75)
 );
 
+create or replace file format garden_plants.veggies.L8_CHALLENGE_FF 
+    TYPE = 'CSV'--csv for comma separated files
+    SKIP_HEADER = 1 --one header row  
+    FIELD_DELIMITER='0x09',
+    TRIM_SPACE=FALSE,
+    FIELD_OPTIONALLY_ENCLOSED_BY=NONE,
+    REPLACE_INVALID_CHARACTERS=TRUE,
+    DATE_FORMAT=AUTO,
+    TIME_FORMAT=AUTO,
+    TIMESTAMP_FORMAT=AUTO
+    ;    
 
---loading data from the S3 stage
-copy into vegetable_details_soil_type
+copy into LU_SOIL_TYPE
 from @util_db.public.like_a_window_into_an_s3_bucket
-files = ( 'VEG_NAME_TO_SOIL_TYPE_PIPE.txt')
-file_format = ( format_name=GARDEN_PLANTS.VEGGIES.PIPECOLSEP_ONEHEADROW );
+files = ( 'LU_SOIL_TYPE.tsv')
+file_format = ( format_name='L8_CHALLENGE_FF' );
