182147352bf1422f12cb694192468066ef07c250
kamibrenda <mukamibrenda254@gmail.com>
2024-01-02 15:44:13 +0300

    Overview of req. code

--- Veggies task.py
+++ Veggies task.py
@@ -1,25 +1,14 @@
 --creation of table for data loading
-CREATE OR REPLACE table VEGETABLE_DETAILS_PLANT_HEIGHT (
-plant_name	varchar (25),
-UOM	varchar (1),
-Low_End_of_Range	Number(30),
-High_End_of_Range Number(30)
-);
+create or replace table LU_SOIL_TYPE(
+SOIL_TYPE_ID number,	
+SOIL_TYPE varchar(15),
+SOIL_DESCRIPTION varchar(75)
+ );
 
+ --create file format 'L8_CHALENGE_FF'
+ CREATE FILE FORMAT L8_CHALLENGE_FF
 --loading data from the file with the file format to the created table
-COPY INTO VEGETABLE_DETAILS_PLANT_HEIGHT
-FROM '@util_db.public.like_a_window_into_an_s3_bucket'
-FILES = ('veg_plant_height.csv')
-FILE_FORMAT = (
-    TYPE='CSV',
-    SKIP_HEADER=1,
-    FIELD_DELIMITER=',',
-    TRIM_SPACE=FALSE,
-    FIELD_OPTIONALLY_ENCLOSED_BY='"',
-    REPLACE_INVALID_CHARACTERS=TRUE,
-    DATE_FORMAT=AUTO,
-    TIME_FORMAT=AUTO,
-    TIMESTAMP_FORMAT=AUTO
-)
-ON_ERROR=ABORT_STATEMENT
-PURGE=TRUE
+copy into LU_SOIL_TYPE
+from @util_db.public.like_a_window_into_an_s3_bucket
+files = ( 'LU_SOIL_TYPE.tsv')
+file_format = ( format_name=GARDEN_PLANTS.VEGGIES.PIPECOLSEP_ONEHEADROW );
