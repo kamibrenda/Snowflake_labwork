a0c80459662aa35efa14b78d75e02048d504ab43
kamibrenda <mukamibrenda254@gmail.com>
2024-01-03 21:06:30 +0300

    Add. labwork

---
+++ TABLE BOOK SEQUENCE FULL.py
@@ -0,0 +1,51 @@
+// Create a new sequence, this one will be a counter for the book table
+CREATE OR REPLACE SEQUENCE "LIBRARY_CARD_CATALOG"."PUBLIC"."SEQ_BOOK_UID" 
+START 1 
+INCREMENT 1 
+COMMENT = 'Use this to fill in the BOOK_UID everytime you add a row';
+
+// Create the book table and use the NEXTVAL as the 
+// default value each time a row is added to the table
+CREATE OR REPLACE TABLE BOOK
+( BOOK_UID NUMBER DEFAULT SEQ_BOOK_UID.nextval
+ ,TITLE VARCHAR(50)
+ ,YEAR_PUBLISHED NUMBER(4,0)
+);
+
+// Insert records into the book table
+// You don't have to list anything for the
+// BOOK_UID field because the default setting
+// will take care of it for you
+INSERT INTO BOOK(TITLE,YEAR_PUBLISHED)
+VALUES
+ ('Food',2001)
+,('Food',2006)
+,('Food',2008)
+,('Food',2016)
+,('Food',2015);
+
+// Create the relationships table
+// this is sometimes called a "Many-to-Many table"
+CREATE TABLE BOOK_TO_AUTHOR
+(  BOOK_UID NUMBER
+  ,AUTHOR_UID NUMBER
+);
+
+//Insert rows of the known relationships
+INSERT INTO BOOK_TO_AUTHOR(BOOK_UID,AUTHOR_UID)
+VALUES
+ (1,1)  // This row links the 2001 book to Fiona Macdonald
+,(1,2)  // This row links the 2001 book to Gian Paulo Faleschini
+,(2,3)  // Links 2006 book to Laura K Egendorf
+,(3,4)  // Links 2008 book to Jan Grover
+,(4,5)  // Links 2016 book to Jennifer Clapp
+,(5,6); // Links 2015 book to Kathleen Petelinsek
+
+//Check your work by joining the 3 tables together
+//You should get 1 row for every author
+select * 
+from book_to_author ba 
+join author a 
+on ba.author_uid = a.author_uid 
+join book b 
+on b.book_uid=ba.book_uid; 
---
+++ seq_reset.py
@@ -0,0 +1,14 @@
+//Drop and recreate the counter (sequence) so that it starts at 3 
+CREATE OR REPLACE SEQUENCE "LIBRARY_CARD_CATALOG"."PUBLIC"."SEQ_AUTHOR_UID" 
+START 3 
+INCREMENT 1 
+COMMENT = 'Use this to fill in the AUTHOR_UID every time you add a row';
+
+//Add the remaining author records and use the nextval function instead 
+//of putting in the numbers
+INSERT INTO AUTHOR(AUTHOR_UID,FIRST_NAME,MIDDLE_NAME, LAST_NAME) 
+Values
+(SEQ_AUTHOR_UID.nextval, 'Laura', 'K','Egendorf')
+,(SEQ_AUTHOR_UID.nextval, 'Jan', '','Grover')
+,(SEQ_AUTHOR_UID.nextval, 'Jennifer', '','Clapp')
+,(SEQ_AUTHOR_UID.nextval, 'Kathleen', '','Petelinsek');
