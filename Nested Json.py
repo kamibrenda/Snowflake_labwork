9a792211bfd453d20b16d6dd6fcc9b3391fcba61
kamibrenda <mukamibrenda254@gmail.com>
2024-01-04 10:08:43 +0300

    Nested Json file

---
+++ Nested Json.py
@@ -0,0 +1,45 @@
+// Create an Ingestion Table for the NESTED JSON Data
+CREATE OR REPLACE TABLE LIBRARY_CARD_CATALOG.PUBLIC.NESTED_INGEST_JSON 
+(
+  "RAW_NESTED_BOOK" VARIANT
+);
+
+//see the full file path
+list  @util_db.public.like_a_window_into_an_s3_bucket;
+
+//loading the Json file
+copy into NESTED_INGEST_JSON
+from @util_db.public.like_a_window_into_an_s3_bucket
+files = ('json_book_author_nested.json')
+file_format = ( format_name='JSON_FILE_FORMAT' );
+
+
+//a few queries
+SELECT RAW_NESTED_BOOK
+FROM NESTED_INGEST_JSON;
+
+SELECT RAW_NESTED_BOOK:year_published
+FROM NESTED_INGEST_JSON;
+
+SELECT RAW_NESTED_BOOK:authors
+FROM NESTED_INGEST_JSON;
+
+//Use the flatten command to explore flattening the nested book and author data
+SELECT value:first_name
+FROM NESTED_INGEST_JSON
+,LATERAL FLATTEN(input => RAW_NESTED_BOOK:authors);
+
+SELECT value:first_name
+FROM NESTED_INGEST_JSON
+,table(flatten(RAW_NESTED_BOOK:authors));
+
+//Add a CAST command to the fields returned
+SELECT value:first_name::VARCHAR, value:last_name::VARCHAR
+FROM NESTED_INGEST_JSON
+,LATERAL FLATTEN(input => RAW_NESTED_BOOK:authors);
+
+//Assign new column  names to the columns using "AS"
+SELECT value:first_name::VARCHAR AS FIRST_NM
+, value:last_name::VARCHAR AS LAST_NM
+FROM NESTED_INGEST_JSON
+,LATERAL FLATTEN(input => RAW_NESTED_BOOK:authors);
