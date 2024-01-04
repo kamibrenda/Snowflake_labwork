8f1793c85794124950561d71e8c93bbec2bae9f4
kamibrenda <mukamibrenda254@gmail.com>
2024-01-04 10:09:27 +0300

    Twitter data pipeline

---
+++ Twitter data pipeline.py
@@ -0,0 +1,105 @@
+//Create a new database to hold the Twitter file
+CREATE DATABASE SOCIAL_MEDIA_FLOODGATES 
+COMMENT = 'There\'s so much data from social media - flood warning';
+
+USE DATABASE SOCIAL_MEDIA_FLOODGATES;
+
+//Create a table in the new database
+CREATE or REPLACE TABLE SOCIAL_MEDIA_FLOODGATES.PUBLIC.TWEET_INGEST 
+("RAW_STATUS" VARIANT) 
+COMMENT = 'Bring in tweets, one row per tweet or status entity';
+
+//Create a JSON file format in the new database
+CREATE OR REPLACE FILE FORMAT SOCIAL_MEDIA_FLOODGATES.PUBLIC.JSON_FILE_FORMAT 
+TYPE = 'JSON' 
+COMPRESSION = 'AUTO' 
+ENABLE_OCTAL = FALSE 
+ALLOW_DUPLICATE = FALSE 
+STRIP_OUTER_ARRAY = TRUE 
+STRIP_NULL_VALUES = FALSE 
+IGNORE_UTF8_ERRORS = FALSE;
+
+lIST @util_db.public.like_a_window_into_an_s3_bucket;
+
+//loading the Json file
+copy into PUBLIC.TWEET_INGEST 
+from @util_db.public.like_a_window_into_an_s3_bucket
+files = ('nutrition_tweets.json')
+file_format = ( format_name='JSON_FILE_FORMAT' );
+
+//select statements as seen in the video
+SELECT RAW_STATUS
+FROM TWEET_INGEST;
+
+SELECT RAW_STATUS:entities
+FROM TWEET_INGEST;
+
+SELECT RAW_STATUS:entities:hashtags
+FROM TWEET_INGEST;
+
+//Explore looking at specific hashtags by adding bracketed numbers
+//This query returns just the first hashtag in each tweet
+SELECT RAW_STATUS:entities:hashtags[0].text
+FROM TWEET_INGEST;
+
+//This version adds a WHERE clause to get rid of any tweet that 
+//doesn't include any hashtags
+SELECT RAW_STATUS:entities:hashtags[0].text
+FROM TWEET_INGEST
+WHERE RAW_STATUS:entities:hashtags[0].text is not null;
+
+//Perform a simple CAST on the created_at key
+//Add an ORDER BY clause to sort by the tweet's creation date
+SELECT RAW_STATUS:created_at::DATE
+FROM TWEET_INGEST
+ORDER BY RAW_STATUS:created_at::DATE;
+
+//Flatten statements that return the whole hashtag entity
+SELECT value
+FROM TWEET_INGEST
+,LATERAL FLATTEN
+(input => RAW_STATUS:entities:hashtags);
+
+SELECT value
+FROM TWEET_INGEST
+,TABLE(FLATTEN(RAW_STATUS:entities:hashtags));
+
+//Flatten statement that restricts the value to just the TEXT of the hashtag
+SELECT value:text
+FROM TWEET_INGEST
+,LATERAL FLATTEN
+(input => RAW_STATUS:entities:hashtags);
+
+
+//Flatten and return just the hashtag text, CAST the text as VARCHAR
+SELECT value:text::VARCHAR
+FROM TWEET_INGEST
+,LATERAL FLATTEN
+(input => RAW_STATUS:entities:hashtags);
+
+//Flatten and return just the hashtag text, CAST the text as VARCHAR
+// Use the AS command to name the column
+SELECT value:text::VARCHAR AS THE_HASHTAG
+FROM TWEET_INGEST
+,LATERAL FLATTEN
+(input => RAW_STATUS:entities:hashtags);
+
+//Add the Tweet ID and User ID to the returned table
+SELECT RAW_STATUS:user:id AS USER_ID
+,RAW_STATUS:id AS TWEET_ID
+,value:text::VARCHAR AS HASHTAG_TEXT
+FROM TWEET_INGEST
+,LATERAL FLATTEN
+(input => RAW_STATUS:entities:hashtags);
+
+
+//normalise the hashtags
+create or replace view SOCIAL_MEDIA_FLOODGATES.PUBLIC.HASHTAGS_NORMALIZED as
+(SELECT RAW_STATUS:user:id AS USER_ID
+,RAW_STATUS:id AS TWEET_ID
+,value:text::VARCHAR AS HASHTAG_TEXT
+FROM TWEET_INGEST
+,LATERAL FLATTEN
+(input => RAW_STATUS:entities:hashtags)
+);
+
